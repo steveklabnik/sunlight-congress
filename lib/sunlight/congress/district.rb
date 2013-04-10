922b4e1bd210a8dd944d8ec02ec2c6bbf691ba2c
Robert J Samson <rjsamson@me.com>
2013-04-09 20:37:16 -0400

    Add district location search by zip and lat / long
    Adds coverage for the districts section of the sunlight-congress API

---
+++ lib/sunlight/congress/district.rb
@@ -0,0 +1,27 @@
+require 'net/http'
+require 'json'
+
+module Sunlight
+  module Congress
+    class District
+      attr_accessor :state, :district
+
+      def initialize(options)
+        self.state = options["state"]
+        self.district = options["district"]
+      end
+
+      def self.by_zipcode(zipcode)
+        uri = URI("http://congress.api.sunlightfoundation.com/districts/locate?zip=#{zipcode}&apikey=#{Sunlight::Congress.api_key}")
+
+        new(JSON.load(Net::HTTP.get(uri))["results"].first)
+      end
+
+      def self.by_latlong(latitude, longitude)
+        uri = URI("http://congress.api.sunlightfoundation.com/districts/locate?latitude=#{latitude}&longitude=#{longitude}&apikey=#{Sunlight::Congress.api_key}")
+
+        new(JSON.load(Net::HTTP.get(uri))["results"].first)
+      end
+    end
+  end
+end
