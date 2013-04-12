e6e87005db1cea701164975deb7a2a67b9a97193
Aaron Chambers <achambers@gmail.com>
2013-04-12 17:51:45 +0100

    Modified class definition for consistency

--- lib/sunlight/congress/district.rb
+++ lib/sunlight/congress/district.rb
@@ -1,26 +1,22 @@
 require 'sunlight/congress'
 
-module Sunlight
-  module Congress
-    class District
-      attr_accessor :state, :district
+class Sunlight::Congress::District
+  attr_accessor :state, :district
 
-      def initialize(options)
-        self.state = options["state"]
-        self.district = options["district"]
-      end
+  def initialize(options)
+    self.state = options["state"]
+    self.district = options["district"]
+  end
 
-      def self.by_zipcode(zipcode)
-        uri = URI("#{Sunlight::Congress::BASE_URI}/districts/locate?zip=#{zipcode}&apikey=#{Sunlight::Congress.api_key}")
+  def self.by_zipcode(zipcode)
+    uri = URI("#{Sunlight::Congress::BASE_URI}/districts/locate?zip=#{zipcode}&apikey=#{Sunlight::Congress.api_key}")
 
-        new(JSON.load(Net::HTTP.get(uri))["results"].first)
-      end
+    new(JSON.load(Net::HTTP.get(uri))["results"].first)
+  end
 
-      def self.by_latlong(latitude, longitude)
-        uri = URI("#{Sunlight::Congress::BASE_URI}/districts/locate?latitude=#{latitude}&longitude=#{longitude}&apikey=#{Sunlight::Congress.api_key}")
+  def self.by_latlong(latitude, longitude)
+    uri = URI("#{Sunlight::Congress::BASE_URI}/districts/locate?latitude=#{latitude}&longitude=#{longitude}&apikey=#{Sunlight::Congress.api_key}")
 
-        new(JSON.load(Net::HTTP.get(uri))["results"].first)
-      end
-    end
+    new(JSON.load(Net::HTTP.get(uri))["results"].first)
   end
 end
