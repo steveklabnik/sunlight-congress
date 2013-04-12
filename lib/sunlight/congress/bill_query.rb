d946128a978d2eaa04cf68c126145b5c34f2e9a7
Robert J Samson <rjsamson@me.com>
2013-04-11 20:52:30 -0400

    Add BillQuery to search bills, filter, and page through results

---
+++ lib/sunlight/congress/bill_query.rb
@@ -0,0 +1,71 @@
+require 'sunlight/congress'
+
+class Sunlight::Congress::BillQuery
+  attr_accessor :count, :per_page, :current_page, :current_page_count,
+                :pages, :results
+
+  def initialize(response, uri)
+    @uri                = uri
+    @results            = response['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
+    @count              = response['count']
+    @per_page           = response['page']['per_page']
+    @current_page       = response['page']['page']
+    @current_page_count = response['page']['count']
+    @pages          = ((@count / @per_page) + 0.5).round
+  end
+
+  def page(page_num)
+    return false unless page_num <= @pages
+
+    new_uri = URI("#{@uri}&page=#{page_num}")
+    JSON.load(Net::HTTP.get(new_uri))['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
+  end
+
+  def page!(page_num)
+    return false unless page_num <= @pages
+
+    new_uri = URI("#{@uri}&page=#{page_num}")
+    response = JSON.load(Net::HTTP.get(new_uri))
+
+    @current_page = response['page']['page']
+    @current_page_count = response['page']['count']
+    @results = response['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
+  end
+
+  def next_page!
+    return false unless (@current_page + 1) <= @pages
+
+    @current_page += 1
+    new_uri = URI("#{@uri}&page=#{@current_page}")
+    response = JSON.load(Net::HTTP.get(new_uri))
+
+    @current_page_count = response['page']['count']
+    @results = response['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
+  end
+
+  def self.search(query, filters = {})
+    args = process_filters(filters)
+    uri = URI(URI.escape("#{Sunlight::Congress::BASE_URI}/bills/search?query=\"#{query}\"&apikey=#{Sunlight::Congress.api_key}#{args}"))
+
+    new(JSON.load(Net::HTTP.get(uri)), uri)
+  end
+
+  def self.by_fields(filters = {})
+    args = process_filters(filters)
+    uri = URI(URI.escape("#{Sunlight::Congress::BASE_URI}/bills?apikey=#{Sunlight::Congress.api_key}#{args}"))
+
+    new(JSON.load(Net::HTTP.get(uri)), uri)
+  end
+
+  private
+  def self.process_filters(filters)
+    opts = ""
+    if filters.any?
+      filters.each do |k,v|
+        opts << "&#{k}=#{v}"
+      end
+    end
+
+    opts
+  end
+end
