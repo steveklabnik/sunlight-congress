8b2eecd96b708e9ab51f7de2476fef53ee4d0850
Ben Hamill <git-commits@benhamill.com>
2013-04-12 23:51:26 -0500

    Failing test case for legislator API.

--- test/integration/legislators_test.rb
+++ test/integration/legislators_test.rb
@@ -1,16 +1,16 @@
 require 'sunlight/congress'
 require 'webmock/minitest'
 
-class TestIntegrationCongress < MiniTest::Unit::TestCase
+class TestIntegrationCongressLegislator < MiniTest::Unit::TestCase
   def setup
-    Sunlight::Congress.api_key = "thisismykey"
+    @api = Sunlight::Congress.new("thisismykey")
   end
 
   def test_legislators_by_zipcode
     stub_request(:get, "http://congress.api.sunlightfoundation.com/legislators/locate?apikey=thisismykey&zip=90210")
       .to_return(body: '{"results":[{"first_name":"Joe"}]}')
 
-    legislators = Sunlight::Congress::Legislator.by_zipcode(90210)
+    legislators = @api.legislator.by_zipcode(90210)
 
     assert_equal "Joe", legislators.first.first_name
   end
@@ -19,7 +19,7 @@ class TestIntegrationCongress < MiniTest::Unit::TestCase
     stub_request(:get, "http://congress.api.sunlightfoundation.com/legislators/locate?apikey=thisismykey&latitude=42.6525&longitude=-73.7567")
       .to_return(body: '{"results":[{"first_name":"Joe"}]}')
 
-    legislators = Sunlight::Congress::Legislator.by_latlong(42.6525, -73.7567)
+    legislators = @api.legislator.by_latlong(42.6525, -73.7567)
 
     assert_equal "Joe", legislators.first.first_name
   end
