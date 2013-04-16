801b2501fc03b0a1c58e1b18cc860f49d1b35ff7
Ben Hamill <git-commits@benhamill.com>
2013-04-16 00:07:12 -0500

    Refactor bill queries.

--- lib/sunlight/congress/bill_query.rb
+++ lib/sunlight/congress/bill_query.rb
@@ -1,59 +1,73 @@
 require 'sunlight/congress'
 
-class Sunlight::Congress::BillQuery
-  attr_accessor :count, :per_page, :current_page, :current_page_count,
-                :pages, :results
-
-  def initialize(response, uri)
-    @uri                = uri
-    @results            = response['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
-    @count              = response['count']
-    @per_page           = response['page']['per_page']
-    @current_page       = response['page']['page']
-    @current_page_count = response['page']['count']
-    @pages          = ((@count / @per_page) + 0.5).round
+class Sunlight::Congress
+  def bill_query
+    Sunlight::Congress::BillQuery::Finder.new(self.api_key)
   end
 
-  def page(page_num)
-    return false unless page_num <= @pages
+  class BillQuery
+    attr_accessor :count, :per_page, :current_page, :current_page_count,
+                  :pages, :results
 
-    new_uri = URI("#{@uri}&page=#{page_num}")
-    JSON.load(Net::HTTP.get(new_uri))['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
-  end
+    def initialize(response, uri)
+      @uri                = uri
+      @results            = response['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
+      @count              = response['count']
+      @per_page           = response['page']['per_page']
+      @current_page       = response['page']['page']
+      @current_page_count = response['page']['count']
+      @pages          = ((@count / @per_page) + 0.5).round
+    end
 
-  def page!(page_num)
-    return false unless page_num <= @pages
+    def page(page_num)
+      return false unless page_num <= @pages
 
-    new_uri = URI("#{@uri}&page=#{page_num}")
-    response = JSON.load(Net::HTTP.get(new_uri))
+      new_uri = URI("#{@uri}&page=#{page_num}")
+      JSON.load(Net::HTTP.get(new_uri))['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
+    end
 
-    @current_page = response['page']['page']
-    @current_page_count = response['page']['count']
-    @results = response['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
-  end
+    def page!(page_num)
+      return false unless page_num <= @pages
 
-  def next_page!
-    return false unless (@current_page + 1) <= @pages
+      new_uri = URI("#{@uri}&page=#{page_num}")
+      response = JSON.load(Net::HTTP.get(new_uri))
 
-    @current_page += 1
-    new_uri = URI("#{@uri}&page=#{@current_page}")
-    response = JSON.load(Net::HTTP.get(new_uri))
+      @current_page = response['page']['page']
+      @current_page_count = response['page']['count']
+      @results = response['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
+    end
 
-    @current_page_count = response['page']['count']
-    @results = response['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
-  end
+    def next_page!
+      return false unless (@current_page + 1) <= @pages
 
-  def self.search(query, filters = {})
-    args = filters.inject("") { |str, arr| str << "&#{arr[0]}=#{arr[1]}" }
-    uri = URI(URI.escape("#{Sunlight::Congress::BASE_URI}/bills/search?query=\"#{query}\"&apikey=#{Sunlight::Congress.api_key}#{args}"))
+      @current_page += 1
+      new_uri = URI("#{@uri}&page=#{@current_page}")
+      response = JSON.load(Net::HTTP.get(new_uri))
 
-    new(JSON.load(Net::HTTP.get(uri)), uri)
-  end
+      @current_page_count = response['page']['count']
+      @results = response['results'].collect { |json| Sunlight::Congress::Bill.new(json) }
+    end
 
-  def self.by_fields(filters = {})
-    args = filters.inject("") { |str, arr| str << "&#{arr[0]}=#{arr[1]}" }
-    uri = URI(URI.escape("#{Sunlight::Congress::BASE_URI}/bills?apikey=#{Sunlight::Congress.api_key}#{args}"))
+    class Finder
+      attr_reader :api_key
 
-    new(JSON.load(Net::HTTP.get(uri)), uri)
+      def initialize(api_key)
+        @api_key = api_key
+      end
+
+      def search(query, filters = {})
+        args = filters.inject("") { |str, arr| str << "&#{arr[0]}=#{arr[1]}" }
+        uri = URI(URI.escape("#{Sunlight::Congress::BASE_URI}/bills/search?query=\"#{query}\"&apikey=#{api_key}#{args}"))
+
+        Sunlight::Congress::BillQuery.new(JSON.load(Net::HTTP.get(uri)), uri)
+      end
+
+      def by_fields(filters = {})
+        args = filters.inject("") { |str, arr| str << "&#{arr[0]}=#{arr[1]}" }
+        uri = URI(URI.escape("#{Sunlight::Congress::BASE_URI}/bills?apikey=#{api_key}#{args}"))
+
+        Sunlight::Congress::BillQuery.new(JSON.load(Net::HTTP.get(uri)), uri)
+      end
+    end
   end
 end
--- test/integration/bill_query_test.rb
+++ test/integration/bill_query_test.rb
@@ -1,16 +1,16 @@
 require 'sunlight/congress'
 require 'webmock/minitest'
 
-class TestIntegrationCongress < MiniTest::Unit::TestCase
+class TestIntegrationCongressBillQuery < MiniTest::Unit::TestCase
   def setup
-    Sunlight::Congress.api_key = "specialapikey"
+    @api = Sunlight::Congress.new("thisismykey")
   end
 
   def test_bill_query_search
     stub_request(:get, "http://congress.api.sunlightfoundation.com/bills/search?apikey=thisismykey&query=%22health%20care%22")
       .to_return(body: File.new('test/integration/json/bill_query/health_care.json'))
 
-    bills = Sunlight::Congress::BillQuery.search("health care")
+    bills = @api.bill_query.search("health care")
 
     assert_equal "Patient Protection and Affordable Care Act", bills.results[0].short_title
   end
@@ -22,7 +22,7 @@ class TestIntegrationCongress < MiniTest::Unit::TestCase
     stub_request(:get, "http://congress.api.sunlightfoundation.com/bills/search?apikey=thisismykey&query=%22health%20care%22&page=2")
       .to_return(body: File.new('test/integration/json/bill_query/health_care_page_2.json'))
 
-    bills = Sunlight::Congress::BillQuery.search("health care")
+    bills = @api.bill_query.search("health care")
     bills.next_page!
 
     assert_equal 2, bills.current_page
@@ -35,7 +35,7 @@ class TestIntegrationCongress < MiniTest::Unit::TestCase
     stub_request(:get, "http://congress.api.sunlightfoundation.com/bills/search?apikey=thisismykey&query=%22health%20care%22&page=7")
       .to_return(body: File.new('test/integration/json/bill_query/health_care_page_7.json'))
 
-    bills = Sunlight::Congress::BillQuery.search("health care")
+    bills = @api.bill_query.search("health care")
     bills.page!(7)
 
     assert_equal 7, bills.current_page
@@ -45,7 +45,7 @@ class TestIntegrationCongress < MiniTest::Unit::TestCase
     stub_request(:get, "http://congress.api.sunlightfoundation.com/bills/search?apikey=thisismykey&query=%22health%20care%22&bill_type=s")
       .to_return(body: File.new('test/integration/json/bill_query/health_care_bill_type_s.json'))
 
-    bills = Sunlight::Congress::BillQuery.search("health care", :bill_type => 's')
+    bills = @api.bill_query.search("health care", :bill_type => 's')
 
     assert_equal 992, bills.count
     assert_equal 's', bills.results[0].bill_type
@@ -55,7 +55,7 @@ class TestIntegrationCongress < MiniTest::Unit::TestCase
     stub_request(:get, "http://congress.api.sunlightfoundation.com/bills?apikey=thisismykey&bill_type=s&congress=111&history.active=true")
       .to_return(body: File.new('test/integration/json/bill_query/fields.json'))
 
-    bills = Sunlight::Congress::BillQuery.by_fields(:bill_type => 's', :congress => 111, 'history.active' => 'true')
+    bills = @api.bill_query.by_fields(:bill_type => 's', :congress => 111, 'history.active' => 'true')
 
     assert_equal 22, bills.count
   end
