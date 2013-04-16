fe276331995143a4d12c313912341521698bb4df
Ben Hamill <git-commits@benhamill.com>
2013-04-16 00:02:04 -0500

    Refactor bills.

--- lib/sunlight/congress/bill.rb
+++ lib/sunlight/congress/bill.rb
@@ -1,68 +1,90 @@
 require 'sunlight/congress'
 
-class Sunlight::Congress::Bill
-  attr_accessor :title, :bill_id, :bill_type, :number, :congress, :chamber,
-    :introduced_on, :last_action_at, :last_vote_at, :last_version_on,
-    :sponsor_id, :nicknames, :urls, :committee_ids, :official_title,
-    :cosponsors_count, :history, :short_title, :enacted_as, :search,
-    :popular_title, :related_bill_ids, :last_version,
-    :withdrawn_cosponsors_count
-
-  def initialize(options)
-    self.title                      = options["title"]
-    self.bill_id                    = options["bill_id"]
-    self.bill_type                  = options["bill_type"]
-    self.number                     = options["number"]
-    self.congress                   = options["congress"]
-    self.chamber                    = options["chamber"]
-    self.introduced_on              = options["introduced_on"]
-    self.last_action_at             = options["last_action_at"]
-    self.last_vote_at               = options["last_vote_at"]
-    self.last_version_on            = options["last_version_on"]
-    self.nicknames                  = options["nicknames"]
-    self.sponsor_id                 = options["sponsor_id"]
-    self.urls                       = OpenStruct.new(options["urls"])
-    self.committee_ids              = options["committee_ids"]
-    self.official_title             = options["official_title"]
-    self.cosponsors_count           = options["cosponsors_count"]
-    self.history                    = OpenStruct.new(options["history"])
-    self.short_title                = options["short_title"]
-    self.enacted_as                 = OpenStruct.new(options["enacted_as"])
-    self.popular_title              = options["popular_title"]
-    self.related_bill_ids           = options["related_bill_ids"]
-    self.last_version               = OpenStruct.new(options["last_version"])
-    self.last_version.urls          = OpenStruct.new(self.last_version.urls)
-    self.withdrawn_cosponsors_count = options["withdrawn_cosponsors_count"]
-    self.search                     = OpenStruct.new(options["search"])
+class Sunlight::Congress
+  def bill
+    Sunlight::Congress::Bill::Finder.new(self.api_key)
   end
+  class Bill
+    attr_accessor :title, :bill_id, :bill_type, :number, :congress, :chamber,
+      :introduced_on, :last_action_at, :last_vote_at, :last_version_on,
+      :sponsor_id, :nicknames, :urls, :committee_ids, :official_title,
+      :cosponsors_count, :history, :short_title, :enacted_as, :search,
+      :popular_title, :related_bill_ids, :last_version,
+      :withdrawn_cosponsors_count
 
-  def self.by_title(title)
-    uri = URI("#{Sunlight::Congress::BASE_URI}/bills/search?query=#{title}&apikey=#{Sunlight::Congress.api_key}")
+    def initialize(options)
+      self.title                      = options["title"]
+      self.bill_id                    = options["bill_id"]
+      self.bill_type                  = options["bill_type"]
+      self.number                     = options["number"]
+      self.congress                   = options["congress"]
+      self.chamber                    = options["chamber"]
+      self.introduced_on              = options["introduced_on"]
+      self.last_action_at             = options["last_action_at"]
+      self.last_vote_at               = options["last_vote_at"]
+      self.last_version_on            = options["last_version_on"]
+      self.nicknames                  = options["nicknames"]
+      self.sponsor_id                 = options["sponsor_id"]
+      self.urls                       = OpenStruct.new(options["urls"])
+      self.committee_ids              = options["committee_ids"]
+      self.official_title             = options["official_title"]
+      self.cosponsors_count           = options["cosponsors_count"]
+      self.history                    = OpenStruct.new(options["history"])
+      self.short_title                = options["short_title"]
+      self.enacted_as                 = OpenStruct.new(options["enacted_as"])
+      self.popular_title              = options["popular_title"]
+      self.related_bill_ids           = options["related_bill_ids"]
+      self.last_version               = OpenStruct.new(options["last_version"])
+      self.last_version.urls          = OpenStruct.new(self.last_version.urls)
+      self.withdrawn_cosponsors_count = options["withdrawn_cosponsors_count"]
+      self.search                     = OpenStruct.new(options["search"])
+    end
 
-    JSON.load(Net::HTTP.get(uri))["results"].collect{|json| new(json)}
-  end
+    class Finder
+      attr_reader :api_key
 
-  def self.by_congress(congress)
-    uri = URI("http://congress.api.sunlightfoundation.com/bills?congress=#{congress}&apikey=#{Sunlight::Congress.api_key}")
+      def initialize(api_key)
+        @api_key = api_key
+      end
 
-    JSON.load(Net::HTTP.get(uri))["results"].collect{|json| new(json) }
-  end
+      def by_title(title)
+        uri = URI("#{Sunlight::Congress::BASE_URI}/bills/search?query=#{title}&apikey=#{api_key}")
 
-  def self.by_term(term)
-    uri = URI("http://congress.api.sunlightfoundation.com/bills/search?query='#{term}'&apikey=#{Sunlight::Congress.api_key}")
+        JSON.load(Net::HTTP.get(uri))["results"].collect do |json|
+          Sunlight::Congress::Bill.new(json)
+        end
+      end
 
-    JSON.load(Net::HTTP.get(uri))["results"].collect{|json| new(json) }
-  end
+      def by_congress(congress)
+        uri = URI("http://congress.api.sunlightfoundation.com/bills?congress=#{congress}&apikey=#{api_key}")
 
-  def self.by_sponsor_party(party)
-    party = party[0].upcase
-    uri = URI("http://congress.api.sunlightfoundation.com/bills?sponsor.party='#{party}'&apikey=#{Sunlight::Congress.api_key}")
+        JSON.load(Net::HTTP.get(uri))["results"].collect do |json|
+          Sunlight::Congress::Bill.new(json)
+        end
+      end
 
-    JSON.load(Net::HTTP.get(uri))["results"].collect{|json| new(json) }
-  end
+      def by_term(term)
+        uri = URI("http://congress.api.sunlightfoundation.com/bills/search?query='#{term}'&apikey=#{api_key}")
 
-  def self.by_bill_id(bill_id)
-    uri = URI("#{Sunlight::Congress::BASE_URI}/bills?bill_id=#{bill_id}&apikey=#{Sunlight::Congress.api_key}")
-    new(JSON.load(Net::HTTP.get(uri))['results'].first)
+        JSON.load(Net::HTTP.get(uri))["results"].collect do |json|
+          Sunlight::Congress::Bill.new(json)
+        end
+      end
+
+      def by_sponsor_party(party)
+        party = party[0].upcase
+        uri = URI("http://congress.api.sunlightfoundation.com/bills?sponsor.party='#{party}'&apikey=#{api_key}")
+
+        JSON.load(Net::HTTP.get(uri))["results"].collect do |json|
+          Sunlight::Congress::Bill.new(json)
+        end
+      end
+
+      def by_bill_id(bill_id)
+        uri = URI("#{Sunlight::Congress::BASE_URI}/bills?bill_id=#{bill_id}&apikey=#{api_key}")
+
+        Sunlight::Congress::Bill.new(JSON.load(Net::HTTP.get(uri))['results'].first)
+      end
+    end
   end
 end
--- test/integration/bills_test.rb
+++ test/integration/bills_test.rb
@@ -1,9 +1,9 @@
 require 'sunlight/congress'
 require 'webmock/minitest'
 
-class TestIntegrationCongress < MiniTest::Unit::TestCase
+class TestIntegrationCongressBill < MiniTest::Unit::TestCase
   def setup
-    Sunlight::Congress.api_key = "specialapikey"
+    @api = Sunlight::Congress.new("thisismykey")
   end
 
   def test_bills_by_title
@@ -11,7 +11,7 @@ class TestIntegrationCongress < MiniTest::Unit::TestCase
       to_return(:status => 200, :body => '{"results":[{"title":"Awesome Health Care Bill"}]}', :headers => {})
 
     title = "Health"
-    bills = Sunlight::Congress::Bill.by_title(title)
+    bills = @api.bill.by_title(title)
 
     assert_equal "Awesome Health Care Bill", bills.first.title
   end
@@ -19,7 +19,7 @@ class TestIntegrationCongress < MiniTest::Unit::TestCase
   def test_bills_by_id
     stub_request(:get, "http://congress.api.sunlightfoundation.com/bills?bill_id=s668-113&apikey=thisismykey").
           to_return(:status => 200, :body => '{"results":[{"bill_id":"s668-113"}]}')
-    bill = Sunlight::Congress::Bill.by_bill_id("s668-113")
+    bill = @api.bill.by_bill_id("s668-113")
 
     assert_equal "s668-113", bill.bill_id
   end
