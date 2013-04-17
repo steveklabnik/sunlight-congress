56522929ebc7b96f0ae31a8ba0d5d2fc11812105
Ben Hamill <git-commits@benhamill.com>
2013-04-16 23:10:26 -0500

    Add back tests that ensure old API for gem.

--- test/integration/districts_test.rb
+++ test/integration/districts_test.rb
@@ -1,6 +1,34 @@
 require 'sunlight-congress'
 require 'webmock/minitest'
 
+# This stuff is behavior from initial development. It can be removed with a
+# major version bump.
+class TestIntegrationCongressDistrictOLDAPI < MiniTest::Unit::TestCase
+  def setup
+    Sunlight::Congress.api_key = "thisismykey"
+  end
+
+  def test_districts_by_zipcode
+    stub_request(:get, "http://congress.api.sunlightfoundation.com/districts/locate?apikey=thisismykey&zip=12186")
+      .to_return(body: '{"results":[{"state":"NY", "district":20}]}')
+
+    district = Sunlight::Congress::District.by_zipcode(12186)
+
+    assert_equal "NY", district.state
+    assert_equal 20, district.district
+  end
+
+  def test_districts_by_latlong
+    stub_request(:get, "http://congress.api.sunlightfoundation.com/districts/locate?apikey=thisismykey&latitude=42.6525&longitude=-73.7567")
+      .to_return(body: '{"results":[{"state":"NY", "district":20}]}')
+
+    district = Sunlight::Congress::District.by_latlong(42.6525, -73.7567)
+
+    assert_equal "NY", district.state
+    assert_equal 20, district.district
+  end
+end
+
 class TestIntegrationCongressDistrict < MiniTest::Unit::TestCase
   def setup
     @api = Sunlight::Congress.new("thisismykey")
--- test/integration/legislators_test.rb
+++ test/integration/legislators_test.rb
@@ -1,6 +1,32 @@
 require 'sunlight/congress'
 require 'webmock/minitest'
 
+# This stuff is behavior from initial development. It can be removed with a
+# major version bump.
+class TestIntegrationCongressLegislatorOldAPI < MiniTest::Unit::TestCase
+  def setup
+    Sunlight::Congress.api_key = "thisismykey"
+  end
+
+  def test_legislators_by_zipcode
+    stub_request(:get, "http://congress.api.sunlightfoundation.com/legislators/locate?apikey=thisismykey&zip=90210")
+      .to_return(body: '{"results":[{"first_name":"Joe"}]}')
+
+    legislators = Sunlight::Congress::Legislator.by_zipcode(90210)
+
+    assert_equal "Joe", legislators.first.first_name
+  end
+
+  def test_districts_by_latlong
+    stub_request(:get, "http://congress.api.sunlightfoundation.com/legislators/locate?apikey=thisismykey&latitude=42.6525&longitude=-73.7567")
+      .to_return(body: '{"results":[{"first_name":"Joe"}]}')
+
+    legislators = Sunlight::Congress::Legislator.by_latlong(42.6525, -73.7567)
+
+    assert_equal "Joe", legislators.first.first_name
+  end
+end
+
 class TestIntegrationCongressLegislator < MiniTest::Unit::TestCase
   def setup
     @api = Sunlight::Congress.new("thisismykey")
